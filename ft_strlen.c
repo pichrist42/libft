63038fa11e6bbf4dbc3edfccfe9b23ed40ccdbdf
Pierre Christaller <pichrist42@gmail.com>
2017-01-30 04:08:10 +0100

    strlen strnequ

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: pichrist <pichrist@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/08/05 15:07:54 by pichrist          #+#    #+#             */
-/*   Updated: 2017/01/30 02:14:03 by pichrist         ###   ########.fr       */
+/*   Updated: 2017/01/30 03:30:28 by pichrist         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,6 @@ size_t	ft_strlen(const char *str)
 {
 	const char *c;
 
-	if (!str)
-		return (0);
 	c = str;
 	while (*c)
 		c++;
--- ft_strnequ.c
+++ ft_strnequ.c
@@ -6,7 +6,7 @@
 /*   By: pichrist <pichrist@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/29 17:35:47 by pichrist          #+#    #+#             */
-/*   Updated: 2017/01/30 01:05:27 by pichrist         ###   ########.fr       */
+/*   Updated: 2017/01/30 03:53:40 by pichrist         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,7 @@
 
 int		ft_strnequ(char const *s1, char const *s2, size_t n)
 {
+	if (!s1 || !s2)
+		return (0);
 	return (!ft_strncmp(s1, s2, n));
 }
